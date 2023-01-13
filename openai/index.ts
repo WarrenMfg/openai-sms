708594dd68f9e389667918d1b8da03115427f011
WarrenMfg <warrenmfg@icloud.com>
2023-01-13 17:13:22 -0500

    init

---
+++ openai/index.ts
@@ -0,0 +1,11 @@
+import { AzureFunction, Context } from '@azure/functions';
+
+const eventGridTrigger: AzureFunction = async function (
+  context: Context,
+  eventGridEvent: any
+): Promise<void> {
+  context.log(typeof eventGridEvent);
+  context.log(eventGridEvent);
+};
+
+export default eventGridTrigger;
