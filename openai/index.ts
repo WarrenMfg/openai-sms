0b06b3722333bb9c6fff3cff812e0fa0fff3dc6c
WarrenMfg <warrenmfg@icloud.com>
2023-01-14 06:24:13 -0500

    adds openai and sms libs

--- openai/index.ts
+++ openai/index.ts
@@ -1,11 +1,35 @@
 import { AzureFunction, Context } from '@azure/functions';
+import { Configuration, OpenAIApi } from 'openai';
+import { SmsClient } from '@azure/communication-sms';
 
 const eventGridTrigger: AzureFunction = async function (
   context: Context,
   eventGridEvent: any
 ): Promise<void> {
-  context.log(typeof eventGridEvent);
-  context.log(eventGridEvent);
+  if (eventGridEvent.data.from !== process.env.MY_CELL) return;
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const client = new SmsClient(process.env.COMM_SERVICE_CONN_STR);
+
+  try {
+    const { data } = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: eventGridEvent.data.message,
+      temperature: +process.env.OPENAI_TEMPERATURE,
+      max_tokens: +process.env.MAX_TOKENS,
+    });
+
+    await client.send({
+      from: process.env.COMM_SERVICE,
+      to: [process.env.MY_CELL],
+      message: data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default eventGridTrigger;
