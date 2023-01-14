1e605077be41fb08cb602ce2a265fc0f4b9764f8
WarrenMfg <warrenmfg@icloud.com>
2023-01-14 07:10:57 -0500

    adds trycatches; moves log

--- openai/index.ts
+++ openai/index.ts
@@ -6,13 +6,19 @@ const eventGridTrigger: AzureFunction = async function (
   context: Context,
   eventGridEvent: any
 ): Promise<void> {
-  if (eventGridEvent.data.from !== process.env.MY_CELL) return;
+  if (
+    eventGridEvent.data.from !== process.env.MY_CELL ||
+    !eventGridEvent.data.message
+  )
+    return;
 
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const client = new SmsClient(process.env.COMM_SERVICE_CONN_STR);
+
+  let message: string | undefined;
 
   try {
     const { data } = await openai.createCompletion({
@@ -22,16 +28,24 @@ const eventGridTrigger: AzureFunction = async function (
       max_tokens: +process.env.OPENAI_MAX_TOKENS,
     });
 
-    console.log(data.choices[0].text);
+    message = data.choices[0].text.trim();
+  } catch (error) {
+    console.log(error.message, error.stack);
+    return;
+  }
 
+  try {
     await client.send({
       from: process.env.COMM_SERVICE,
       to: [process.env.MY_CELL],
-      message: data.choices[0].text,
+      message,
     });
   } catch (error) {
-    console.error(error);
+    console.log(error.message, error.stack);
+    return;
   }
+
+  console.log(message);
 };
 
 export default eventGridTrigger;
